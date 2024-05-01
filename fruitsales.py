04e5e4bd0eb3f48bb219658b92e7dfb41f67792c
Andrei Miron <victormiron.home@gmail.com>
2024-05-01 18:44:15 -0400

    flowers are not fruits

--- fruitsales.py
+++ fruitsales.py
@@ -2,6 +2,6 @@
 # add your code here
 import pandas as pd
 
-flower = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]}, index=['2017 Sales', '2018 Sales'])
+fruits = pd.DataFrame({'Apples': [35, 41], 'Bananas': [21, 34]}, index=['2017 Sales', '2018 Sales'])
 
-flower.to_csv("fruit.csv", sep=',', encoding='utf-8', index=True)
+fruits.to_csv("fruit.csv", sep=',', encoding='utf-8', index=True)
